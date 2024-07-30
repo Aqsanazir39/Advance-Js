1933a6134b3deedb5d2eadb5fac6ca1e97821c72
Aqsa Nazir <aqsanazir39@gmail.com>
2024-07-30 12:46:23 +0500

    New files

---
+++ 01_string_temp.js
@@ -0,0 +1,11 @@
+let user = "Aqsa Nazir";
+let marks = 345;
+console.log( ` Hello " ${user} " from Abbottabad` );
+
+var firstName = "Aqsa";
+var secName = "Nazir";
+function fullName(){
+  return `${firstName} ${secName}`;
+}
+let hello = `Hello ${fullName(firstName,secName)}`;
+console.log(hello);
